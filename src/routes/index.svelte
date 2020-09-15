153cbc06d58bb5e48c1d902f26aa5e4fe8a68da5
afendi <afendihamat@gmail.com>
2020-09-16 06:36:35 +0800

    new tagline

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
 
 <div class="overflow-hidden shadow-lg border-t-4 bg-white mb-4 rounded-b-lg rounded-t border-red-light w-full">
     <div class="px-6 py-4 mb-2 mt-4 mb-8">
-        <div class="uppercase tracking-wide text-c2 mb-4">Video dan Poster Kesihatan Mulut</div>
+        <div class="uppercase tracking-wide text-c2 mb-4">Info Ringkas Kesihatan Mulut dan Penjagaan Diabetes</div>
         <a class="flex cursor-pointer border px-4 py-2 text-lg text-grey-darkest border-b-0 hover:bg-blue-400 hover:text-white" href="penjagaan-optimum">
             
             <div class="pl-2">Penjagaan optimum untuk kesihatan mulut</div>
